f7f9215f08106c4d2206c25da3951229313a24ce
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-14 12:11:33 +0500

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import startingApp from "./startingApp.js";
 import displayTitleAndTagline from "./displayTitle.js";
 import convertCurrency from "./convertCurrency.js";
 import useAppAgain from "./useAppAgain.js";
+import getCurrencyData from "./getCurrencyData.js";
 
 async function main() {
   await startingApp();
   await displayTitleAndTagline();
+  await getCurrencyData();
   await convertCurrency();
-  await useAppAgain();
+  useAppAgain();
 }
 
 main();
