b5932a5a6724a132273918efcd0ef27233672c2d
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-14 12:11:13 +0500

    conversion rates

--- src/conversionRates.ts
+++ src/conversionRates.ts
@@ -1,8 +1,23 @@
-const conversionRates = {
+type ConversionRates = {
+  USD: number;
+  PKR: number;
+  SAR: number;
+  EUR: number;
+  GBP: number;
+  JPY: number;
+  CAD: number;
+  AUD: number;
+};
+
+const conversionRates: ConversionRates = {
   USD: 1,
-  PKR: 250,
-  SAR: 3.75,
-  EUR: 0.92,
+  PKR: 0.0064,
+  SAR: 0.2666,
+  EUR: 1.2321,
+  GBP: 1.3728,
+  JPY: 0.0091,
+  CAD: 0.7984,
+  AUD: 0.7713,
 };
 
 export default conversionRates;
